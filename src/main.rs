2130f0d7b7974649ccec21c961cb0d1302e10121
Guido Giuntoli <gagiuntoli@gmail.com>
2022-04-22 22:51:02 +0200

    Starting tutorial

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::env;
+use std::fs;
+use std::process;
+use std::error::Error;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    println!("search for = {}", config.query);
+    println!("filename = {}", config.filename);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content = fs::read_to_string(config.filename)?;
+    println!("{}", content);
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config {query, filename})
+    }
+}
